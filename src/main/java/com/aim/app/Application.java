548ca36fa7f577f2c2fe3d971d33945d6123abe9
aim-akonya <akonya@adtel.co.ke>
2021-05-31 16:01:54 +0300

    socket server

--- src/main/java/com/aim/app/Application.java
+++ src/main/java/com/aim/app/Application.java
@@ -1,8 +1,14 @@
 package com.aim.app;
 
+import com.aim.app.sockets.SocketServer;
+
 public class Application {
 
-	public static void main(String[] args) {
-		System.out.println("Application Startup Reached");
+	public static void main(String[] args)  {
+		
+		SocketServer server = new SocketServer(8080);
+		
+		server.startServer();
+
 	}
 }
