6b30b906f3c4a744cb2c73cac025b454988dbdce
aim-akonya <akonya@adtel.co.ke>
2021-08-26 17:18:46 +0300

    updates

--- src/main/java/com/aim/app/ipscan/services/IpScanService.java
+++ src/main/java/com/aim/app/ipscan/services/IpScanService.java
@@ -1,8 +1,12 @@
 package com.aim.app.ipscan.services;
 
+import java.io.IOException;
+import java.net.SocketException;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public interface IpScanService {
 
+	void whois() throws SocketException, IOException;
 }
--- src/main/java/com/aim/app/ipscan/services/IpScanServiceImpl.java
+++ src/main/java/com/aim/app/ipscan/services/IpScanServiceImpl.java
@@ -3,6 +3,11 @@
  */
 package com.aim.app.ipscan.services;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.SocketException;
+
+import org.apache.commons.net.whois.WhoisClient;
 import org.springframework.stereotype.Service;
 
 /**
@@ -11,6 +16,20 @@ import org.springframework.stereotype.Service;
  */
 
 @Service
-public class IpScanServiceImpl {
+public class IpScanServiceImpl implements IpScanService {
+
+	private WhoisClient whois;
+
+	public IpScanServiceImpl() {
+		whois = new WhoisClient();
+	}
+
+	@Override
+	public void whois()
+			throws SocketException, IOException{
+		whois.connect(WhoisClient.DEFAULT_HOST);
+		whois.query("");
+		whois.disconnect();
+	}
 
 }
